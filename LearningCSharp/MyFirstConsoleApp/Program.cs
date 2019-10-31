5ddd9c5f65d7530c81c9288abca7684050214858
Raees Malek <raees@ix.co.z>
2019-10-31 10:28:06 +0200

    Created Hello World Application

--- LearningCSharp/MyFirstConsoleApp/Program.cs
+++ LearningCSharp/MyFirstConsoleApp/Program.cs
@@ -10,6 +10,8 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
         }
     }
 }
